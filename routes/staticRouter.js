8bba29e1ae89fbf89297f7258898938337f34a3c
Mansi Patel <108953768+thesassyblogger@users.noreply.github.com>
2025-05-16 15:44:16 -0600

    Update staticRouter.js

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     const allurls = await URL.find({ });
-        console.log('All URLs:', allUrls);  // ✅ Debug log
+        console.log('All URLs:', allurls);  // ✅ Debug log
 
     return res.render('home', {
         urls: allurls,
